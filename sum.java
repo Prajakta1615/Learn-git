b6486b6b9c66cfd5ee8f1ab36c3185dbc6da563f
Prajakta Vibhute <hello@praju.com>
2023-07-21 01:37:08 +0530

    added message in sum.java

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class arithmetic
+{
+public static void main(String[]args){
+float a=20.5;b=6.4;
+System.out.println("a+b="+ (a+b));
+System.out.println("a-b="+ (a-b));
+System.out.println("a*b="+ (a*b));
+System.out.println("a/b="+ (a/b));
+!!!
+}
+}
